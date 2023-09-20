25ec7e60c59a8098234e822c68202801d64b6464
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-09-20 12:33:35 -0400

    added dropdown menu

--- Js/script.js
+++ Js/script.js
@@ -54,3 +54,19 @@ function fetchAnimals(e) {
     // Handle the display of animal data here, e.g., rendering it on the web page
     console.log(animals);
   }
+
+/* NavBar Dropdown Menu*/
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
